5566ad7eb7aad7a562134ec9337570ee2f357ed3
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2024-12-02 20:35:28 +0100

    01 - Historian Histeria Part 2

--- 01HIstorianHisteria/Program.cs
+++ 01HIstorianHisteria/Program.cs
@@ -1,29 +1,59 @@
 ﻿Console.WriteLine("01 - Historian Histeria");
 
+
 string inputPath = "../../../input.csv";
 int distance = 0;
-using (StreamReader sr = new StreamReader(inputPath))
+int similarity = 0;
+
+(List<int> list1, List<int> list2) = await ReadInput(inputPath);
+
+distance = Distance(list1, list2);
+similarity = Similarity(list1, list2);
+
+
+Console.WriteLine($"Distance = {distance}");
+Console.WriteLine($"Similarity = {similarity}");
+
+static async Task<(List<int>, List<int>)> ReadInput(string inputPath)
 {
-    List<int> list1 = new List<int>();
-    List<int> list2 = new List<int>();
-
-    while (!sr.EndOfStream)
+    List<int> list1 = [], list2 = [];
+    using (StreamReader sr = new StreamReader(inputPath))
     {
-        string? line = await sr.ReadLineAsync();
-        string[] splitted = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-        int a = int.Parse(splitted[0]);
-        int b = int.Parse(splitted[1]);
-        list1.Add(a);
-        list2.Add(b);
+        while (!sr.EndOfStream)
+        {
+            string? line = await sr.ReadLineAsync();
+            string[] splitted = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int a = int.Parse(splitted[0]);
+            int b = int.Parse(splitted[1]);
+            list1.Add(a);
+            list2.Add(b);
+        }
     }
+    return (list1, list2);
+}
 
+static int Distance(List<int> list1, List<int> list2)
+{
     IEnumerable<int> sorted1 = list1.Order();
     IEnumerable<int> sorted2 = list2.Order();
+    int distance = 0;
 
-    for(int i = 0; i < sorted1.Count(); i++)
+    for (int i = 0; i < sorted1.Count(); i++)
     {
         distance += Math.Abs(sorted1.ElementAt(i) - sorted2.ElementAt(i));
     }
+
+    return distance;
 }
 
-Console.WriteLine($"Distance = {distance}");
+static int Similarity(List<int> list1, List<int> list2)
+{
+    int similarity = 0;
+
+    foreach(int i in list1)
+    {
+        similarity += i * list2.Count(n => n == i);
+    }
+
+    return similarity;
+}
